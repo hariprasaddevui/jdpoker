5d49564b118b3bd4c03042a85ea47603e71be7cf
leeahari.ravirala <leelahari.ravirala@pragmaticplay.com>
2024-10-16 16:09:49 +0530

    jd changes

---
+++ my-app/src/App.js
@@ -0,0 +1,67 @@
+import logo from "./logo.svg";
+// import "./App.css";
+import "./App3.css";
+// import "./App2.css";
+
+function App() {
+  return (
+    <div className="mainBg">
+      <div className="gameWrapper">
+        <div className="cardHolder">
+          <div className="cardItem">
+            <div className="cardPlaceHolder">
+              <img src="https://i.ibb.co/80B4R4g/Flat-Cards-2.png" alt="" />
+            </div>
+            <div className="dHolder">
+              <div className="decisionIcon">
+                  <img src="https://i.ibb.co/N9wmBgK/Hold-no.png" alt="" />
+              </div>
+            </div>
+          </div>
+          <div className="cardItem">
+            <div className="cardPlaceHolder">
+              <img src="https://i.ibb.co/80B4R4g/Flat-Cards-2.png" alt="" />
+            </div>
+            <div className="dHolder">
+              <div className="decisionIcon">
+                  <img src="https://i.ibb.co/N9wmBgK/Hold-no.png" alt="" />
+              </div>
+            </div>
+          </div>
+          <div className="cardItem">
+            <div className="cardPlaceHolder">
+              <img src="https://i.ibb.co/80B4R4g/Flat-Cards-2.png" alt="" />
+            </div>
+            <div className="dHolder">
+              <div className="decisionIcon">
+                  <img src="https://i.ibb.co/N9wmBgK/Hold-no.png" alt="" />
+              </div>
+            </div>
+          </div>
+          <div className="cardItem">
+            <div className="cardPlaceHolder">
+              <img src="https://i.ibb.co/80B4R4g/Flat-Cards-2.png" alt="" />
+            </div>
+            <div className="dHolder">
+              <div className="decisionIcon">
+                  <img src="https://i.ibb.co/N9wmBgK/Hold-no.png" alt="" />
+              </div>
+            </div>
+          </div>
+          <div className="cardItem">
+            <div className="cardPlaceHolder">
+              <img src="https://i.ibb.co/80B4R4g/Flat-Cards-2.png" alt="" />
+            </div>
+            <div className="dHolder">
+              <div className="decisionIcon">
+                  <img src="https://i.ibb.co/N9wmBgK/Hold-no.png" alt="" />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
